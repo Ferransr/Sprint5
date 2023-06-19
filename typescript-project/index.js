0dc6abdb22bfd95789d86e42e8f52994aaf3ddcf
Ferransr <65186873+Ferransr@users.noreply.github.com>
2023-06-19 12:28:43 +0200

    Nivel1_ok, Nivel2_ex5_ok
    falta Nivel2_ex4 (Buscar una API del tiempo e implementarla)

--- typescript-project/index.js
+++ typescript-project/index.js
@@ -8,14 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function mostrarChiste() {
-    const botonScore = document.getElementById('boton-score');
-    if (botonScore) {
-        botonScore.style.display = 'block';
-    }
-}
+let chiste;
 ;
-const llamarChiste = () => __awaiter(void 0, void 0, void 0, function* () {
+const reportJokes = [];
+const mostrarBotones = () => {
+    const botonesScore = document.querySelectorAll(".boton-score");
+    botonesScore.forEach((boton) => {
+        boton.style.display = "inline-block";
+    });
+};
+const ocultarBotones = () => {
+    const botonesScore = document.querySelectorAll(".boton-score");
+    botonesScore.forEach((boton) => {
+        boton.style.display = "none";
+    });
+};
+const llamarChiste1 = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const respuesta = yield fetch("https://icanhazdadjoke.com", {
             headers: {
@@ -24,10 +32,42 @@ const llamarChiste = () => __awaiter(void 0, void 0, void 0, function* () {
         });
         const datos = yield respuesta.json();
         document.getElementById("texto-chiste").innerHTML = datos.joke;
-        console.log(datos);
+        chiste = datos.joke;
+        console.log('JOKE=>', datos);
     }
     catch (error) {
         console.log("Error al llamar a la API:", error);
     }
-    mostrarChiste();
+    mostrarBotones();
 });
+const llamarChiste2 = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const respuesta = yield fetch("https://api.chucknorris.io/jokes/random");
+        const datos = yield respuesta.json();
+        document.getElementById("texto-chiste").innerHTML = datos.value;
+        chiste = datos.value;
+        console.log('JOKE2=>', datos);
+    }
+    catch (error) {
+        console.log("Error al llamar a la API:", error);
+    }
+    mostrarBotones();
+});
+const llamarChistes = () => {
+    const random = Math.trunc(Math.random() * 2);
+    console.log(random);
+    if (random <= 0)
+        llamarChiste1();
+    else
+        llamarChiste2();
+};
+function puntuacion(score) {
+    let report = {
+        joke: chiste,
+        score: score,
+        date: new Date().toISOString(),
+    };
+    reportJokes.push(report);
+    console.table(reportJokes);
+    ocultarBotones();
+}
--- typescript-project/index.ts
+++ typescript-project/index.ts
@@ -1,11 +1,21 @@
-function mostrarChiste(): void {
-  const botonScore: HTMLElement | null = document.getElementById('boton-score');
-  if (botonScore) {
-    botonScore.style.display = 'block';
-  }
-};
+let chiste : string;
+interface tipoChiste { joke: string; score: number; date: string };
+const reportJokes: tipoChiste[] = [];
 
-const llamarChiste = async () => {
+const mostrarBotones = () => {
+  const botonesScore: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".boton-score");
+  botonesScore.forEach((boton: HTMLButtonElement) => {
+    boton.style.display = "inline-block";
+  });
+}
+const ocultarBotones = () => {
+  const botonesScore: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".boton-score");
+  botonesScore.forEach((boton: HTMLButtonElement) => {
+    boton.style.display = "none";
+  });
+}
+
+const llamarChiste1 = async () => {
   try {
     const respuesta = await fetch("https://icanhazdadjoke.com", {
       headers: {
@@ -15,9 +25,42 @@ const llamarChiste = async () => {
     
     const datos = await respuesta.json();
     document.getElementById("texto-chiste")!.innerHTML = datos.joke;
-    console.log(datos);
+    chiste = datos.joke;
+    console.log('JOKE=>',datos);
+
   } catch (error) {
     console.log("Error al llamar a la API:", error);
   }
-  mostrarChiste();
+  mostrarBotones();
 };
+const llamarChiste2 = async () => {
+  try {
+    const respuesta = await fetch("https://api.chucknorris.io/jokes/random");
+    const datos = await respuesta.json();
+    document.getElementById("texto-chiste")!.innerHTML = datos.value;
+    chiste = datos.value;
+    console.log('JOKE2=>',datos);
+
+  } catch (error) {
+    console.log("Error al llamar a la API:", error);
+  }
+  mostrarBotones();
+};
+
+const llamarChistes = () => {
+  const random : number = Math.trunc(Math.random() * 2);
+  console.log(random);
+  if (random <= 0) llamarChiste1();
+  else llamarChiste2();
+};
+
+function puntuacion(score: number) :void {
+  let report: tipoChiste = {
+    joke : chiste,
+    score: score,
+    date: new Date().toISOString(),
+  };
+  reportJokes.push(report);
+  console.table(reportJokes);
+  ocultarBotones();
+}
